9e52558185cf13a250f113311c008bf10dc15c92
amir <imaniai@amazon.com>
2021-06-18 12:49:27 -0400

    scraper script'

--- lambda-scraper/app/scraper/scraper.py
+++ lambda-scraper/app/scraper/scraper.py
@@ -1,160 +1,99 @@
-import hashlib
-import io
 import logging
-import os
 import time
-import requests
+import hashlib
+import base64
+from tqdm import tqdm
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(process)d --- %(name)s %(funcName)20s() : %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 
 class GearScraper:
     logger = logging.getLogger('GearScraper')
 
     def __init__(self):
-        self._tmp_folder = '/tmp/img-scrpr-chrm/'
-        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=self.__get_default_chrome_options())
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    def get_board_full_html(self, gender):
+    def parse(self, gender):
+
+        board_list = []
+        single_board_dict = {}
+
+        url_list = self.__get_boards_url(gender, self.driver)
+        for url in tqdm(url_list[:2], desc="Getting ratings..."):
+            single_board_dict['id'] = self.__hashme(url)
+            single_board_dict['ratings'] = self.__get_ratings(url, self.driver)
+            single_board_dict['meta_data'] = self.__get_meta_data(url, self.driver)
+            board_list.append(single_board_dict)
+        self.logger.info("All boards have been processed.")
+        #         driver.close()
+
+        return board_list
+
+    @staticmethod
+    def __hashme(x):
+        return base64.b64encode(hashlib.sha1(x.encode('UTF-8')).digest())
+
+    def __get_boards_url(self, gender, browser):
         search_url = f"https://thegoodride.com/snowboard-reviews/?{gender}=1"
-        self.driver.get(search_url)
+        board_urls = []
 
+        browser.get(search_url)
+        # click and wait
+        button = browser.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/form/div[1]/div[42]/a')
+        browser.execute_script("arguments[0].click();", button)
+        self.logger.info(f"Loading main page for \033[1m{gender}\033[0m boards...")
+        time.sleep(10)
 
+        elems = browser.find_elements_by_xpath('//*[@id="applications"]/*/a[@href]')
+        for elem in tqdm(elems, desc="Fetching links "):
+            board_urls.append(elem.get_attribute("href"))
 
-    def get_image_urls(self, query: str, max_urls: int, sleep_between_interactions: int = 1):
-        search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
-        self.driver.get(search_url.format(q=query))
+        return board_urls
 
-        image_urls = set()
-        image_count = 0
-        results_start = 0
-        while image_count < max_urls:
-            self.__scroll_to_end(sleep_between_interactions)
-            thumbnail_results = self.driver.find_elements_by_css_selector("img.Q4LuWd")
-            number_results = len(thumbnail_results)
-            self.logger.info(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+    @staticmethod
+    def __get_ratings(url, driver):
+        rating_dict = {}
+        driver.get(url)
 
-            for img in thumbnail_results[results_start:number_results]:
-                self.__click_and_wait(img, sleep_between_interactions)
-                self.__add_image_urls_to_set(image_urls)
-                image_count = len(image_urls)
-                if image_count >= max_urls:
-                    self.logger.info(f"Found: {len(image_urls)} image links, done!")
-                    break
-            else:
-                self.logger.info(f"Found: {len(image_urls)} image links, looking for more ...")
+        # top-right table
+        elems = driver.find_elements_by_xpath('//*[@id="post-"]/div[1]/div[2]/div[1]/table/tbody/*')
+        for row in elems:
+            k = row.find_elements_by_tag_name("td")[0]
+            v = row.find_elements_by_tag_name("td")[1]
+            rating_dict[k.text] = v.text
 
-                load_more_button = self.driver.find_element_by_css_selector(".mye4qd")
-                if load_more_button:
-                    self.logger.info("loading more...")
-                    self.driver.execute_script("document.querySelector('.mye4qd').click();")
+        # top-left table
+        elems = driver.find_elements_by_xpath('//*[@id="post-"]/div[1]/div[2]/div[2]/table/tbody/*')
+        for item in [e.text.split() for e in elems]:
+            rating_dict[item[0]] = item[1]
 
-            # move the result startpoint further down
-            results_start = len(thumbnail_results)
+        # bottom table
+        elems = driver.find_elements_by_xpath('//*[@id="post-"]/div[1]/div[2]/div[3]/*/table/tbody/*')
+        for item in [e.text.split() for e in elems]:
+            rating_dict[item[0]] = item[1]
+        return rating_dict
 
-        return image_urls
+    @staticmethod
+    def __get_meta_data(url, driver):
 
-    def persist_image(self, folder_path: str, url: str):
-        image_content = self.__download_image_content(url)
-        try:
-            image_file = io.BytesIO(image_content)
-            image = Image.open(image_file).convert('RGB')
-            file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
-            with open(file_path, 'wb') as f:
-                image.save(f, "JPEG", quality=85)
-            self.logger.info(f"SUCCESS - saved {url} - as {file_path}")
-        except Exception as e:
-            self.logger.error(f"ERROR - Could not save {url} - {e}")
+        meta_dict = {}
 
-    def get_in_memory_image(self, url: str, format: str):
-        image_content = self.__download_image_content(url)
-        image_hash = hashlib.sha1(image_content).hexdigest()[:10] + '.jpeg'
-        try:
-            image_file = io.BytesIO(image_content)
-            pil_image = Image.open(image_file).convert('RGB')
-            in_mem_file = io.BytesIO()
-            pil_image.save(in_mem_file, format=format)
-            return in_mem_file.getvalue(), image_hash
-        except Exception as e:
-            self.logger.error(f"Could not get image data: {e}")
+        driver.get(url)
+        meta_dict['name'] = url.rsplit('/', 1)[1]
+        meta_dict['url'] = url
+
+        # Get price data
+        for e in driver.find_elements_by_xpath('//*[@id="post-"]/div[1]/div[1]/div/div[1]'):
+            meta_dict['price'] = e.text.rsplit(' ', 1)[1]
+
+        # get image url
+        for e in driver.find_elements_by_xpath('//*[@id="post-"]/div[1]/div[1]/div/div[2]/a'):
+            meta_dict['image_url'] = e.get_attribute("href")
+
+        return meta_dict
 
     def close_connection(self):
         self.driver.quit()
-
-    def __download_image_content(self, url):
-        try:
-            return requests.get(url).content
-        except Exception as e:
-            self.logger.error(f"ERROR - Could not download {url} - {e}")
-
-    def __scroll_to_end(self, sleep_time):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_time)
-
-    def __click_and_wait(self, img, wait_time):
-        try:
-            img.click()
-            time.sleep(wait_time)
-        except Exception:
-            return
-
-    def __add_image_urls_to_set(self, image_urls: set):
-        actual_images = self.driver.find_elements_by_css_selector('img.n3VNCb')
-        for actual_image in actual_images:
-            if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                image_urls.add(actual_image.get_attribute('src'))
-
-    def __get_default_chrome_options(self):
-        chrome_options = webdriver.ChromeOptions()
-
-        lambda_options = [
-            '--autoplay-policy=user-gesture-required',
-            '--disable-background-networking',
-            '--disable-background-timer-throttling',
-            '--disable-backgrounding-occluded-windows',
-            '--disable-breakpad',
-            '--disable-client-side-phishing-detection',
-            '--disable-component-update',
-            '--disable-default-apps',
-            '--disable-dev-shm-usage',
-            '--disable-domain-reliability',
-            '--disable-extensions',
-            '--disable-features=AudioServiceOutOfProcess',
-            '--disable-hang-monitor',
-            '--disable-ipc-flooding-protection',
-            '--disable-notifications',
-            '--disable-offer-store-unmasked-wallet-cards',
-            '--disable-popup-blocking',
-            '--disable-print-preview',
-            '--disable-prompt-on-repost',
-            '--disable-renderer-backgrounding',
-            '--disable-setuid-sandbox',
-            '--disable-speech-api',
-            '--disable-sync',
-            '--disk-cache-size=33554432',
-            '--hide-scrollbars',
-            '--ignore-gpu-blacklist',
-            '--ignore-certificate-errors',
-            '--metrics-recording-only',
-            '--mute-audio',
-            '--no-default-browser-check',
-            '--no-first-run',
-            '--no-pings',
-            '--no-sandbox',
-            '--no-zygote',
-            '--password-store=basic',
-            '--use-gl=swiftshader',
-            '--use-mock-keychain',
-            '--single-process',
-            '--headless']
-
-        #chrome_options.add_argument('--disable-gpu')
-        for argument in lambda_options:
-            chrome_options.add_argument(argument)
-        chrome_options.add_argument('--user-data-dir={}'.format(self._tmp_folder + '/user-data'))
-        chrome_options.add_argument('--data-path={}'.format(self._tmp_folder + '/data-path'))
-        chrome_options.add_argument('--homedir={}'.format(self._tmp_folder))
-        chrome_options.add_argument('--disk-cache-dir={}'.format(self._tmp_folder + '/cache-dir'))
-
-        return chrome_options
